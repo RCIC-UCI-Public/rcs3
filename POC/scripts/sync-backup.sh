fdf6329b09f59e266c37048c56086f4109369c18
Philip Papadopoulos <ppapadop@uci.edu>
2023-06-01 16:45:55 -0700

    Create a template rclone.conf (can be sedited to create common endpoints of s3-backup, s3-inventory Modify sync-backup to allow selection of files, directories. Support DRYRUN=--dry-run to test put a .gitignore in the outputs directory so we don't mistakenly check in credentials.

--- POC/scripts/sync-backup.sh
+++ POC/scripts/sync-backup.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
+# Don't to glob expansion, want the --filter file to interpret
+set -o noglob
 # What to backup - change these for each backup set
 SRCPATH=/pub/ppapadop
+## An array of FILES relative to the top-level SRCPATH
+SRC_FILES=(/*)
+## and Array of DIRECTORIES relative to the top-level SRCPATH
 SRC_FOLDERS=(phylo tdlong-test)
+
+## Should be unique among backup jobs on this host
 BACKUP_NAME=backup-1
 
 # Where to back it up
-ENDPOINT=s3-tmpstore1
+ENDPOINT=s3-backup
 DESTPATH=${BACKUP_NAME}${SRCPATH}
 
 MYDIR=$(dirname $(realpath $0))
@@ -15,9 +22,13 @@ RC_CONFIG_FILE=$HOME/.config/rclone/rclone.conf
 
 # Create a filter file to include the subfolders above
 INCTMP=$(mktemp --suffix=rc-include)
+for x in ${SRC_FILES[*]}; do
+    echo "+ $x" >> $INCTMP
+done
 for x in ${SRC_FOLDERS[*]}; do
     echo "+ $x/**" >> $INCTMP
 done
+# catchall to exclude everything else
 echo "- **" >> $INCTMP
 # Create a logfile
 LOGTMP=$(mktemp /tmp/$BACKUP_NAME-XXX.log)
@@ -25,9 +36,9 @@ LOGTMP=$(mktemp /tmp/$BACKUP_NAME-XXX.log)
 RC_FLAGS_GLOBAL="--metadata --links --config $RC_CONFIG_FILE"
 RC_INCLUDE="--filter-from $INCTMP"
 
-echo rclone sync --log-file $LOGTMP  $RC_FLAGS_GLOBAL $RC_INCLUDE $SRCPATH $ENDPOINT:$DESTPATH
+echo rclone $DRYRUN sync --log-file $LOGTMP  $RC_FLAGS_GLOBAL $RC_INCLUDE $SRCPATH $ENDPOINT:$DESTPATH
 echo "=== RCLONE START: $(date)" > $LOGTMP 
-rclone sync --log-file $LOGTMP  --log-level INFO $RC_FLAGS_GLOBAL $RC_INCLUDE $SRCPATH $ENDPOINT:$DESTPATH
+rclone $DRYRUN sync --log-file $LOGTMP  --log-level INFO $RC_FLAGS_GLOBAL $RC_INCLUDE $SRCPATH $ENDPOINT:$DESTPATH
 # Remove the temporary file
 /bin/rm $INCTMP
 echo "=== RCLONE END: $(date)" >> $LOGTMP 
