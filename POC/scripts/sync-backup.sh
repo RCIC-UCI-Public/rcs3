5f12424fbf27f0361374ee442e84486d777c5bcb
Philip Papadopoulos <ppapadop@uci.edu>
2023-05-31 18:48:46 -0700

    Log to  a file instead of --progress

--- POC/scripts/sync-backup.sh
+++ POC/scripts/sync-backup.sh
@@ -19,13 +19,17 @@ for x in ${SRC_FOLDERS[*]}; do
     echo "+ $x/**" >> $INCTMP
 done
 echo "- **" >> $INCTMP
+# Create a logfile
+LOGTMP=$(mktemp /tmp/$BACKUP_NAME-XXX.log)
 ## Global rclone flags
 RC_FLAGS_GLOBAL="--metadata --links --config $RC_CONFIG_FILE"
 RC_INCLUDE="--filter-from $INCTMP"
 
-echo rclone sync --progress $RC_FLAGS_GLOBAL $RC_INCLUDE $SRCPATH $ENDPOINT:$DESTPATH
-rclone sync --progress $RC_FLAGS_GLOBAL $RC_INCLUDE $SRCPATH $ENDPOINT:$DESTPATH
-
+echo rclone sync --log-file $LOGTMP  $RC_FLAGS_GLOBAL $RC_INCLUDE $SRCPATH $ENDPOINT:$DESTPATH
+echo "=== RCLONE START: $(date)" > $LOGTMP 
+rclone sync --log-file $LOGTMP  --log-level INFO $RC_FLAGS_GLOBAL $RC_INCLUDE $SRCPATH $ENDPOINT:$DESTPATH
 # Remove the temporary file
 /bin/rm $INCTMP
+echo "=== RCLONE END: $(date)" >> $LOGTMP 
+
 
