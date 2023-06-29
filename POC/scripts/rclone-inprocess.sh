ef5272ad46307b3cc02707a20fabcfe4cb56ceaf
Nick Santucci <nicksan@gmail.com>
2023-06-28 17:01:00 -0700

    rclone-inprocess.sh: look back just 14 days and also check for number of checkers if recent enough

--- POC/scripts/rclone-inprocess.sh
+++ POC/scripts/rclone-inprocess.sh
@@ -2,8 +2,8 @@
 
 TZ=":US/Pacific" date
 
-for F in $( echo /tmp/backup-*.log); do
-	echo "==== $F ==== "
-	tail -5000 $F | fgrep -w Transferred | tail -2
-	echo
+for F in $( find /tmp/*.log -mtime -14 ); do
+        echo "==== $F ==== "
+        tail -5000 $F | fgrep -e Transferred -e Checks | tail -3
+        echo
 done
