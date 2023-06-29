f6c09398810d0e065fc87283fb2872a6a100eaf6
Nick Santucci <nicksan@gmail.com>
2023-06-28 17:15:48 -0700

    rclone-inprocess.sh: more accurate accessment of status

--- POC/scripts/rclone-inprocess.sh
+++ POC/scripts/rclone-inprocess.sh
@@ -2,8 +2,8 @@
 
 TZ=":US/Pacific" date
 
-for F in $( find /tmp/*.log -mtime -14 ); do
+for F in $( find /tmp/*.log -mtime -7 ); do
         echo "==== $F ==== "
-        tail -5000 $F | fgrep -e Transferred -e Checks | tail -3
+        tail -5000 $F | fgrep -e Transferred -A1 -B1 -e Checks -e Errors | tail -4
         echo
 done
