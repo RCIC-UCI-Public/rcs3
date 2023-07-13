7ce7b95f4a4cff1747c96112bdf5b38b9816f5c7
Francisco Lopez <lopez@uci.edu>
2023-07-12 17:55:56 -0700

    expanded error checking

--- POC/scripts/bucket-halt-changes.py
+++ POC/scripts/bucket-halt-changes.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python3.9
+#! /usr/bin/python3
 
 import argparse
 import boto3
@@ -28,15 +28,43 @@ args = p.parse_args()
 if "configfile" in aws:
     os.environ[ "AWS_CONFIG_FILE" ] = aws[ "configfile" ]
 
-
-session = boto3.Session( profile_name=aws[ "profile" ] )
+if "profile" in aws:
+    session = boto3.Session( profile_name=aws[ "profile" ] )
+else:
+    session = boto3
 s3 = session.client( "s3" )
-s3.delete_bucket_lifecycle_configuration(
-        Bucket="{}-{}-uci-bkup-bucket".format( args.user, args.host )
-)
+bucketname = "{}-{}-uci-bkup-bucket".format( args.user, args.host )
+# verify that bucket exists, ignore response, catch exception
+try:
+    s3.head_bucket( Bucket=bucketname )
+except s3.exceptions.ClientError:
+    print( "No S3 bucket: {}".format( bucketname ) )
+    sys.exit(1)
+# try to remove lifecycle policy, okay if not present
+try:
+    s3.delete_bucket_lifecycle_configuration(
+        Bucket=bucketname
+    )
+except AttributeError:
+    print( "No lifecycle present on: {}".format( bucketname ) )
 
 iam = session.client( "iam" )
-iam.deattach_user_policy(
-    UserName="{}-{}-sa".format( args.user, args, host ),
-    PolicyArn="arn:aws:iam::{}:policy/{}-{}-uci-bkup-policy".format( aws[ "accountid" ], args.user, args.host )
-)
+username = "{}-{}-sa".format( args.user, args.host )
+# verify that user exists, ignore response, catch exception
+try:
+    iam.get_user(
+        UserName=username
+    )
+except iam.exceptions.NoSuchEntityException:
+    print( "Account not found: {}".format( username ) )
+    sys.exit(1)
+policyarn = "arn:aws:iam::{}:policy/{}-{}-uci-bkup-policy".format( aws[ "accountid" ], args.user, args.host )
+# try to detach user policy, okay if not attached
+try:
+    iam.detach_user_policy(
+        UserName=username,
+        PolicyArn=policyarn
+    )
+except iam.exceptions.NoSuchEntityException:
+    print( "Policy: {}".format( policyarn ) )
+    print( "not attached to: {}".format( username ) )
