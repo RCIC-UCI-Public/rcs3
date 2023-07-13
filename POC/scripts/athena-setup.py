b3029308b1731bae1537a3c203e58c2956d43850
Francisco Lopez <lopez@uci.edu>
2023-07-12 20:35:08 -0700

    error checking, load schema from file

--- POC/scripts/athena-setup.py
+++ POC/scripts/athena-setup.py
@@ -2,6 +2,7 @@
 
 import argparse
 import boto3
+import datetime
 import os
 import sys
 import yaml
@@ -18,56 +19,70 @@ p.add_argument( "host",
         help="hostname" )
 args = p.parse_args()
 
+if not "schemafile" in aws:
+    print( "Missing schema file in configuation settings" )
+    sys.exit(1)
+
 # override location of .aws/config
 if "configfile" in aws:
     os.environ[ "AWS_CONFIG_FILE" ] = aws[ "configfile" ]
 
-session = boto3.Session( profile_name=aws[ "profile" ] )
+# when run from AWS services, profile is not used
+if "profile" in aws:
+    session = boto3.Session( profile_name=aws[ "profile" ] )
+else:
+    session = boto3
 
-athena_client = session.client( "athena" )
-# create the work group
+s3 = session.client( "s3" )
+# verify that bucket exists, ignore response, catch exception
+bucketname="{}-{}-uci-inventory".format( args.user, args.host )
 try:
-    response = athena_client.create_work_group(
+    s3.head_bucket( Bucket=bucketname )
+except s3.exceptions.ClientError:
+    print( "No S3 bucket: {}".format( bucketname ) )
+    sys.exit(1)
+# verify that hive object exists, ignore response, catch exception
+hivedir = "{1}-{2}-uci-bkup-bucket/{1}-{2}-daily/hive/dt={3}-01-00/".format( args.user, args.host, str(datetime.date.today()) )
+hivesym = "{}symlink.txt".format( hivedir )
+try:
+    s3.head_object(
+        Bucket=bucketname,
+        Key=hivesym
+    )
+except s3.exceptions.NoSuchKey:
+    print( "No key: {}".format( hivesym ) )
+    sys.exit(1)
+
+athena = session.client( "athena" )
+# create the work group, okay if already exists
+try:
+    response = athena.create_work_group(
         Name=args.user,
         Configuration={
             'ResultConfiguration': {
-                'OutputLocation': aws[ "reports" ] + "/" + args.user
+                'OutputLocation': "{}/{}".format( aws[ "reports" ], args.user )
             },
             'EnforceWorkGroupConfiguration': False,
             'PublishCloudWatchMetricsEnabled': True
         }
     )
-except athena_client.exceptions.InvalidRequestException:
+except athena.exceptions.InvalidRequestException:
     pass
 
 # create database in default collection
-response = athena_client.start_query_execution( 
+response = athena.start_query_execution( 
     QueryString="create database if not exists {}".format( args.user ),
     WorkGroup=args.user
 )
 
-loadschema = "CREATE EXTERNAL TABLE cncm( \
-  bucketname string, \
-  filename string, \
-  version_id string, \
-  is_latest boolean, \
-  is_delete_marker boolean, \
-  filesize bigint, \
-  last_modified_date string, \
-  storage_class string \
-) \
-ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde' \
-STORED AS INPUTFORMAT 'org.apache.hadoop.hive.ql.io.SymlinkTextInputFormat' \
-OUTPUTFORMAT  'org.apache.hadoop.hive.ql.io.IgnoreKeyTextOutputFormat' \
-LOCATION 's3://awxu-cncm-uci-inventory/awxu-cncm-uci-bkup-bucket/awxu-cncm-daily/hive/dt=2023-07-04-01-00/' ;"
-
-#print( schema )
 # load hive schema into table
-response = athena_client.start_query_execution( 
-    QueryString=loadschema,
-    QueryExecutionContext={
-        'Database': args.user
-    },
-    WorkGroup=args.user
-)
-print( response[ "QueryExecutionId" ] )
+with open( aws[ "schemafile" ], "r" ) as fp:
+    loadschema = fp.read()
+    response = athena.start_query_execution(
+        QueryString=loadschema.format( "lopez", "mybucket", "keylocation" ),
+        QueryExecutionContext={
+            'Database': args.user
+        },
+        WorkGroup=args.user
+    )
+    print( response[ "QueryExecutionId" ] )
