93a606d8955d3091cb1c98c41f9af8f1f08bab4a
Francisco Lopez <lopez@uci.edu>
2023-08-01 22:18:13 -0700

    number of objects per file limit

--- POC/scripts/select-delete-markers.py
+++ POC/scripts/select-delete-markers.py
@@ -9,7 +9,13 @@ p.add_argument( "inputfile",
         help="JSON file to read from" )
 p.add_argument( "outputfile",
         help="save JSON output, create new file or overwrite existing" )
-p.add_argument( "-i", "--include-objects",
+p.add_argument( "-m", "--maxcount", type=int, default=1000,
+        help="maximum number of objects to include per file" )
+g = p.add_mutually_exclusive_group()
+g.add_argument( "-a", "--all-objects",
+        action="store_true",
+        help="all objects even if no associated delete marker" )
+g.add_argument( "-i", "--include-objects",
         action="store_true",
         help="include original objects along with delete markers" )
 args = p.parse_args()
@@ -22,31 +28,63 @@ except:
     #print( "Unexpected error: %s" % e )
     print( "Error parsing", args.inputfile )
     exit(0)
-sp = open( args.outputfile, "w" )
 objs = []
 objkeys = set()
 
+
+def save_objects( o, f, n ):
+    fn = "{}.{}".format( f, n )
+    fp = open( fn, "w" )
+    wrapper = {}
+    wrapper[ "Objects" ] = o
+    wrapper[ "Quiet" ] = False
+    #json.dump( wrapper, fp , indent=4)
+    json.dump( wrapper, fp )
+    print( "{}".format( fn ) )
+
+
+count = 0
+filen = 1
 for i in jp[ "DeleteMarkers" ]:
     x = {}
-    #print( i[ "Key" ] )
-    #print( i[ "VersionId" ] )
     objkeys.add( i[ "Key" ] )
     x[ "Key" ] =  i[ "Key" ]
     x[ "VersionId" ] =  i[ "VersionId" ]
     objs.append( x )
+    count += 1
+    if count == args.maxcount:
+        save_objects( objs, args.outputfile, filen )
+        objs = []
+        count = 0
+        filen += 1
+
+if args.all_objects:
+    for i in jp[ "Versions" ]:
+        x = {}
+        x[ "Key" ] =  i[ "Key" ]
+        x[ "VersionId" ] =  i[ "VersionId" ]
+        objs.append( x )
+        count += 1
+        if count == args.maxcount:
+            save_objects( objs, args.outputfile, filen )
+            objs = []
+            count = 0
+            filen += 1
 
 if args.include_objects:
     for i in jp[ "Versions" ]:
         if i[ "Key" ] in objkeys:
             x = {}
-            #print( i[ "Key" ] )
-            #print( i[ "VersionId" ] )
             x[ "Key" ] =  i[ "Key" ]
             x[ "VersionId" ] =  i[ "VersionId" ]
             objs.append( x )
+            count += 1
+            if count == args.maxcount:
+                save_objects( objs, args.outputfile, filen )
+                objs = []
+                count = 0
+                filen += 1
 
-wrapper = {}
-wrapper[ "Objects" ] = objs
-wrapper[ "Quiet" ] = False
-json.dump( wrapper, sp , indent=4)
+if count > 0:
+    save_objects( objs, args.outputfile, filen )
 
