64990530431810b7c5ebf01ab81569c8550b2c49
Francisco Lopez <lopez@uci.edu>
2023-08-15 15:54:13 -0700

    rewritten to loop until complete

--- POC/scripts/athena-query-job-status.py
+++ POC/scripts/athena-query-job-status.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python3
+#! /usr/bin/env python3
 
 import argparse
 import boto3
@@ -22,13 +22,18 @@ p.add_argument( "-v", "--verbose", action="store_true",
         help="optional print statements for more detail" )
 p.add_argument( "-o", "--output",
         help="override output file" )
+p.add_argument( "-r", "--runonce", action="store_true", default=False,
+        help="do not loop, run once and exit" )
+p.add_argument( "-s", "--sleepinterval", type=int, default=600,
+        help="override the default number of seconds to sleep" )
 args = p.parse_args()
 
 
+listrunning = []
 listready = []
 listempty = []
 listerror = []
-listother = []
+listrecheck = []
 if args.output:
     results = args.output
 else:
@@ -46,48 +51,72 @@ if "profile" in aws:
 else:
     session = boto3
 
+# read in the list of job ids
 try:
     with open( args.idsfile ) as fp:
-        athena_client = session.client( "athena" )
         for rawid in fp:
-            jobid = rawid.strip()
-            if args.verbose:
-                print( jobid )
-            try:
-                response = athena_client.get_query_runtime_statistics(
-                    QueryExecutionId=jobid
-                )
-                joboutput = response[ "QueryRuntimeStatistics" ][ "OutputStage" ]
-                jobstate = joboutput[ "State" ]
-                jobsize = joboutput[ "OutputBytes" ]
-                if args.verbose:
-                    print( jobstate, jobsize )
-                if jobstate == "FINISHED":
-                    if jobsize > 0:
-                        listready.append( jobid )
-                    else:
-                        listempty.append( jobid )
-                else:
-                    listother.append( jobid )
-            except athena_client.exceptions.InvalidRequestException:
-                listerror.append( jobid )
-                print( "Invalid id: {}".format( jobid ) )
+            listrunning.append( rawid.strip() )
+        if args.verbose:
+            print( listrunning )
 except IOError:
     print( "could not read file: {}".format( args.idsfile ) )
+    sys.exit(-1)
+
+athena_client = session.client( "athena" )
+while len( listrunning ) > 0:
+    for jobid in listrunning:
+        if args.verbose:
+            print( "JobId: {}".format( jobid ) )
+        try:
+            response = athena_client.get_query_execution(
+                QueryExecutionId=jobid
+            )
+            jobstate = response[ "QueryExecution" ][ "Status" ][ "State" ]
+            if args.verbose:
+                print( jobstate )
+            if jobstate == "SUCCEEDED":
+                listready.append( jobid )
+            elif jobstate == "FAILED" or jobstate == "CANCELLED":
+                listerror.append( jobid )
+            else:
+                # in QUEUED or RUNNING state
+                listrecheck.append( jobid )
+        except athena_client.exceptions.InvalidRequestException:
+            listerror.append( jobid )
+            print( "Invalid id: {}".format( jobid ) )
+        if args.runonce:
+            break
+        if len( listrecheck ) > 0:
+            if args.verbose:
+                print( "Sleeping {} seconds".format( args.sleepinterval ) )
+            time.sleep( args.sleepinterval )
+        listrunning = listrecheck
 
 # Save the S3 locations of the results that have finished
 with open( results, "w" ) as f:
     for jobid in listready:
-        response = athena_client.get_query_execution(
+        response = athena_client.get_query_runtime_statistics(
             QueryExecutionId=jobid
         )
-        f.write( response[ "QueryExecution" ][ "ResultConfiguration" ][ "OutputLocation" ] )
-        f.write( "\n" )
+        joboutput = response[ "QueryRuntimeStatistics" ][ "OutputStage" ]
+        jobstate = joboutput[ "State" ]
+        jobsize = joboutput[ "OutputBytes" ]
+        if args.verbose:
+            print( jobstate, jobsize )
+        if jobstate == "FINISHED":
+            if jobsize > 0:
+                response = athena_client.get_query_execution(
+                    QueryExecutionId=jobid
+                )
+                f.write( response[ "QueryExecution" ][ "ResultConfiguration" ][ "OutputLocation" ] )
+                f.write( "\n" )
+            else:
+                listempty.append( jobid )
 
 # Report empty results, errors, and jobs that are not ready
 for i in listempty:
     print( "No results, skipping: {}".format( i ) )
 for i in listerror:
-    print( "Not a valid job id: {}".format( i ) )
-for i in listother:
+    print( "Problem with job id: {}".format( i ) )
+for i in listrecheck:
     print( "Jobs not completed: {}".format( i ) )
