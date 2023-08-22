0e829ed5a6c67bc3d1ee3992f37a968d93863b2a
Philip Papadopoulos <ppapadop@uci.edu>
2023-08-21 18:09:46 -0700

    region should be generic, too

--- POC/scripts/create-rclone-conf.sh
+++ POC/scripts/create-rclone-conf.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 # Creates an rclone.conf file on stdout using the same user host pattern as buckets
-source  <(./aws-settings-to-bash.py)
 MYDIR=$(dirname $(realpath $0))
 TEMPLATES_DIR=$MYDIR/../templates
+source  <($MYDIR/aws-settings-to-bash.py)
 
 if [ $# -ne 2 ] ; then
     echo $0 user host
@@ -14,6 +14,7 @@ host=$2
 cat $TEMPLATES_DIR/rclone.conf | \
 sed s/xxxuserxxx/$user/ | \
 sed s/xxxhostxxx/$host/ | \
+sed s/xxxregionxxx/$RCS3_REGION/ | \
 sed s/xxxbucketxxx/$RCS3_BUCKET_POSTFIX/ | \
 sed s/xxxinventoryxxx/$RCS3_INVENTORY_POSTFIX/
 
