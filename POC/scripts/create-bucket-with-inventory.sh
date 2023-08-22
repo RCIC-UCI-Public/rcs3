8b203a4bba32cdf48d5f953f39c85e298ee539fe
Philip Papadopoulos <ppapadop@uci.edu>
2023-08-22 15:52:36 -0700

    Modify create-bucket-with-inventory.sh and template policies to utilize config variables in aws-settings.yaml

--- POC/scripts/create-bucket-with-inventory.sh
+++ POC/scripts/create-bucket-with-inventory.sh
@@ -1,23 +1,47 @@
-#! /bin/sh
+#! /bin/bash
+# Create the storage bucket and inventory bucket
+# Storage bucket: <user>-<host>-$RCS3_BUCKET_POSTFIX
+# Inventory bucket: <user>-<host>-$RCS3_INVENTORY_POSTFIX
+MYDIR=$(dirname $(realpath $0))
+TEMPLATES_DIR=$MYDIR/../templates
+CONFIG_DIR=$MYDIR/../config
+OUTPUTS_DIR=$MYDIR/../outputs
+
+# READ in the variables in the $CONFIG_DIR/aws-settings.yaml and present as bash variables 
+source  <($MYDIR/aws-settings-to-bash.py)
 
 if [ $# -ne 2 ] ; then
     echo $0 user host
     exit 0
 fi
 
-outputs="outputs"
-templates="templates"
+outputs=$OUTPUTS_DIR
+templates=$TEMPLATES_DIR
 
 user=$1
 host=$2
-# RCIC AWS account
-uciawsacct=774954368688
-awsprofile=774954368688_AWSAdministratorAccess
 
-for bucketname in $user-$host-uci-bkup-bucket $user-$host-uci-inventory
+# AWS account
+awsacct=$RCS3_ACCOUNTID
+awsprofile=$RCS3_PROFILE
+
+function localize {
+# need to replace patterns in generic files to localize
+# localize <inputputfile> <outputfile>
+   local input=$1
+   local output=$2
+   cat $input | \
+   sed s/xxxawsacctxxx/$awsacct/ | \
+   sed -e s/xxxbucketxxx/$RCS3_BUCKET_POSTFIX/g -e s/xxxinventoryxxx/$RCS3_INVENTORY_POSTFIX/g | \
+   sed -e s/xxxpolicyxxx/RCS3_POLICY_POSTFIX/g | \
+   sed s/xxxuserxxx/$user/ | \
+   sed s/xxxhostxxx/$host/ > $output
+}
+
+for bucketname in $user-$host-$RCS3_BUCKET_POSTFIX $user-$host-$RCS3_INVENTORY_POSTFIX
 do
-    #aws --profile $awsprofile s3api create-bucket --bucket $bucketname --object-lock-enabled-for-bucket --region us-west-2 --create-bucket-configuration LocationConstraint=us-west-2
-    aws --profile $awsprofile s3api create-bucket --bucket $bucketname --region us-west-2 --create-bucket-configuration LocationConstraint=us-west-2
+    #aws --profile $awsprofile s3api create-bucket --bucket $bucketname --object-lock-enabled-for-bucket --region $RCS3_REGION --create-bucket-configuration LocationConstraint=$RCS3_REGION
+    aws --profile $awsprofile s3api create-bucket --bucket $bucketname --region $RCS3_REGION --create-bucket-configuration LocationConstraint=$RCS3_REGION
 
     aws --profile $awsprofile s3api put-bucket-encryption --bucket $bucketname --server-side-encryption-configuration '{
     "Rules": [
@@ -36,35 +60,26 @@ do
 done
 
 # put bucket policy for inventory bucket
-cat $templates/template-inventory-permissions.json | \
-sed s/xxxuciawsacctxxx/$uciawsacct/ | \
-sed s/xxxuserxxx/$user/ | \
-sed s/xxxhostxxx/$host/ > $outputs/$user-$host-inv-perm.json
+localize $templates/template-inventory-permissions.json $outputs/$user-$host-inv-perm.json
 
-aws --profile $awsprofile s3api put-bucket-policy --bucket $user-$host-uci-inventory --policy file://$outputs/$user-$host-inv-perm.json
+aws --profile $awsprofile s3api put-bucket-policy --bucket $user-$host-$RCS3_INVENTORY_POSTFIX --policy file://$outputs/$user-$host-inv-perm.json
 
 # put inventory configuration on the primary bucket
-cat $templates/template-inventory-configuration.json | \
-sed s/xxxuciawsacctxxx/$uciawsacct/ | \
-sed s/xxxuserxxx/$user/ | \
-sed s/xxxhostxxx/$host/ > $outputs/$user-$host-inv-cfg.json
+localize $templates/template-inventory-configuration.json $outputs/$user-$host-inv-cfg.json
 
-aws --profile $awsprofile s3api put-bucket-inventory-configuration --bucket $user-$host-uci-bkup-bucket --id $user-$host-daily --inventory-configuration file://$outputs/$user-$host-inv-cfg.json
+aws --profile $awsprofile s3api put-bucket-inventory-configuration --bucket $user-$host-$RCS3_BUCKET_POSTFIX --id $user-$host-daily --inventory-configuration file://$outputs/$user-$host-inv-cfg.json
 
 # put the lifecycle policy into the bucket for storage transititions and permanent deletions
 
-aws --profile $awsprofile s3api put-bucket-lifecycle-configuration --bucket $user-$host-uci-bkup-bucket --lifecycle-configuration file://$templates/lifecycle-all.json
+aws --profile $awsprofile s3api put-bucket-lifecycle-configuration --bucket $user-$host-$RCS3_BUCKET_POSTFIX --lifecycle-configuration file://$templates/lifecycle-all.json
 
 # create service account access policy
-cat $templates/template-policy2.json | \
-sed s/xxxuciawsacctxxx/$uciawsacct/ | \
-sed s/xxxuserxxx/$user/ | \
-sed s/xxxhostxxx/$host/ > $outputs/$user-$host-policy.json
+localize $templates/template-policy2.json $outputs/$user-$host-policy.json
 
-aws --profile $awsprofile iam create-policy --policy-name $user-$host-uci-bkup-policy  --policy-document file://$outputs/$user-$host-policy.json
+aws --profile $awsprofile iam create-policy --policy-name $user-$host-$RCS3_POLICY_POSTFIX  --policy-document file://$outputs/$user-$host-policy.json
 
 aws --profile $awsprofile iam create-user --user-name $user-$host-sa
 
-aws --profile $awsprofile iam attach-user-policy --user-name $user-$host-sa --policy-arn arn:aws:iam::${uciawsacct}:policy/$user-$host-uci-bkup-policy
+aws --profile $awsprofile iam attach-user-policy --user-name $user-$host-sa --policy-arn arn:aws:iam::${awsacct}:policy/$user-$host-$RCS3_POLICY_POSTFIX
 
 aws --profile $awsprofile iam create-access-key --user-name $user-$host-sa | tee $outputs/$user-$host.credentials
