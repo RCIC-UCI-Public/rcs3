e59b92f5e26205f7be057a1b39da262c26bcbde0
Philip Papadopoulos <ppapadop@uci.edu>
2023-08-24 15:44:33 -0700

    aws-settings-to-bash.py now respects the environment variable RCS3_AWS_SETTINGS

--- POC/common/aws-settings-to-bash.py
+++ POC/common/aws-settings-to-bash.py
@@ -3,6 +3,9 @@
 # 1. Keys are RCS3_<all caps of variable name>
 # 2. List elements are comma-separated
 
+# This will read the environment variable RCS3_AWS_SETTINGS, if set
+# it will overwrite the default aws-settings.yaml  filename with RCS3_AWS_SETTINGS 
+
 import yaml
 import sys
 import os
@@ -12,13 +15,26 @@ def main(argv):
     scriptdir=os.path.realpath(os.path.dirname(__file__))
     configdir=os.path.normpath(os.path.join(scriptdir, "..","config"))
 
+    # if RCS3_AWS_SETTINGS is defined, use it
+    try:
+       settingsfile=os.environ['RCS3_AWS_SETTINGS']
+    except:
+       settingsfile="aws-settings.yaml"
+
     usage="Convert aws-settings file to bash variables"
     parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,allow_abbrev=True)
-    parser.add_argument("-s", "--settings", dest="settings", default="aws-settings.yaml", help="Settings file")
+    parser.add_argument("-s", "--settings", dest="settings", default=settingsfile, 
+       help="Settings file (Default:%s)"%settingsfile)
     args = parser.parse_args()
     
     # Read the global configuration settings
-    with open( os.path.join(configdir,args.settings), "r" ) as f:
+    # If a path separator is in the 
+    if os.path.sep in args.settings or os.path.exists(args.settings):
+        yamlfile=args.settings
+    else:
+        yamlfile=os.path.join(configdir,args.settings)
+
+    with open( yamlfile, "r" ) as f:
         aws = yaml.safe_load( f )
      
     for x in aws.keys():
