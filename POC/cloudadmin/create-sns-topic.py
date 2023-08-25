32cc2543dd78f00a1d06ed1336f71f4efe6747da
Philip Papadopoulos <ppapadop@uci.edu>
2023-08-25 13:20:44 -0700

    create-sns-topic moved and updated. Same functionality

--- POC/cloudadmin/create-sns-topic.py
+++ POC/cloudadmin/create-sns-topic.py
@@ -5,9 +5,13 @@ import boto3
 import os
 import sys
 import yaml
+import rcs3functions as rcs3
 
-with open( "config/aws-settings.yaml", "r" ) as f:
-    aws = yaml.safe_load( f )
+scriptdir=os.path.realpath(os.path.dirname(__file__))
+configdir=os.path.normpath(os.path.join(scriptdir, "..","config"))
+templatedir=os.path.normpath(os.path.join(scriptdir, "..","templates","alarms-bucket"))
+
+aws=rcs3.read_aws_settings()
 
 
 usage="Create an SNS topic for email notifications related to a specific S3 bucket. The default action is to add user@uci.edu.  Repeated calls add addresses to the SNS topic.  AWS checks for duplicate addresses.  The subscribed address must confirm by responding to AWS confirmation message."
@@ -32,7 +36,7 @@ session = boto3.Session( profile_name=aws[ "profile" ] )
 
 # check if associated S3 bucket exists
 s3_client = session.client( "s3" )
-bucket = args.user + "-" + args.host + "-uci-bkup-bucket"
+bucket = args.user + "-" + args.host + "-" + aws['bucket_postfix'] 
 try:
     s3_client.head_bucket( Bucket=bucket )
 except s3_client.exceptions.ClientError:
@@ -52,7 +56,7 @@ sns_client = session.client( "sns" )
 
 # no error if topic already exists
 response = sns_client.create_topic(
-    Name="{}-{}-uci-notify".format( args.user, args.host )
+    Name="{}-{}-{}".format( args.user, args.host,aws['owner_notify'] )
 )
 
 for m in maillist:
