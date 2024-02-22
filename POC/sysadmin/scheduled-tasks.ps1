dd67f5af71340b4ddc410d08e0b4bb5d81f95bfe
Philip M. Papadopoulos <ppapadop@uci.edu>
2024-02-21 16:04:44 -0800

    Update scheduled-task.ps1 to use powershell instead of cmd

--- POC/sysadmin/scheduled-tasks.ps1
+++ POC/sysadmin/scheduled-tasks.ps1
@@ -8,7 +8,8 @@ $LOCKFILE="$RCS3ROOT\..\gen-backup.lock"
 $LOGFILE="$RCS3ROOT\..\gen-backup.log"
 $RCLONE="$LOCALBIN\rclone.exe"
 $GENBACKUP="$CWD\..\sysadmin\gen-backup.py"
-
+$COMMONARGS="-WindowStyle Hidden" 
+#
 # When to weekly sync verse daily top up
 $syncDay = 'Sunday'
 $topupDays = 'Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'
@@ -18,7 +19,7 @@ $backupCmd = "$GENBACKUP  --parallel=1 --threads=4 --checkers=64 --rclonecmd=$RC
 
 # Create the Weekly full sync
 $trigger = New-ScheduledTaskTrigger -Weekly -DaysOfWeek $syncDay -At 12:30am
-$action = New-ScheduledTaskAction -WorkingDirectory "$CWD\.." -Execute cmd -Argument "/c python3 $backupCmd >> $LOGFILE 2>&1" 
+$action = New-ScheduledTaskAction -WorkingDirectory "$CWD\.." -Execute powershell.exe -Argument "$COMMONARGS -command python3 $backupCmd >> $LOGFILE 2>&1" 
 Register-ScheduledTask -TaskName 'Backup Weekly Sync' -Action $action -Trigger $trigger
 echo $action
 
@@ -26,6 +27,6 @@ echo $action
 # Create the Daily Top (all days except Weekly Day)
 $trigger = New-ScheduledTaskTrigger -Weekly -DaysOfWeek $topupDays -At 12:30am
 $topupCmd = $backupCmd + " --top-up=24h"
-$action = New-ScheduledTaskAction -WorkingDirectory "$CWD\.." -Execute cmd -Argument "/c python3 $topupCmd >> $LOGFILE 2>&1" 
+$action = New-ScheduledTaskAction -WorkingDirectory "$CWD\.." -Execute powershell.exe -Argument "$COMMONARGS -command python3 $topupCmd >> $LOGFILE 2>&1" 
 Register-ScheduledTask -TaskName 'Backup Daily Top-up' -Action $action -Trigger $trigger
 echo $action
