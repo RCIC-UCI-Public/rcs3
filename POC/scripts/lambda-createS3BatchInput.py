08032437f5b723458d998261f180a195d1c757cb
Francisco Lopez <lopez@uci.edu>
2024-04-16 14:19:14 -0700

    use file name for idempotency token, eTag was failing

--- POC/scripts/lambda-createS3BatchInput.py
+++ POC/scripts/lambda-createS3BatchInput.py
@@ -17,12 +17,14 @@ def lambda_handler(event, context):
                 r = s3.head_object( Bucket=m.group(1), Key=m.group(2) )
                 l.append( {
                     'ResultsFile': arnprefix + m.group(1) + "/" + m.group(2),
-                    'ETag': r[ "ETag" ].strip( '\"' )
+                    'ETag': r[ "ETag" ].strip( '\"' ),
+                    'FileToken': m.group(2)
                 } )
             else:
                 l.append( {
                     'ResultsFile': n[ "ResultsFile" ],
-                    'ETag': ""
+                    'ETag': "",
+                    'FileToken': ""
                 } )
         else:
             # build list of problem files. drop for now
