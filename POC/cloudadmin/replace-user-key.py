12acd663c032d40e908c72c924fe29bd660cb45e
Francisco Lopez <lopez@uci.edu>
2024-04-15 17:45:13 -0700

    updated code to replace service account key

--- POC/cloudadmin/replace-user-key.py
+++ POC/cloudadmin/replace-user-key.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python3
+#! /usr/bin/env python3
 
 import argparse
 import boto3
@@ -7,8 +7,11 @@ import os
 import sys
 import yaml
 
-sys.path.append( "scripts" )
-from commonfunctions import delete_user_keys, create_user_key
+execdir = os.path.dirname(os.path.abspath(__file__))
+basedir = os.path.dirname( execdir )
+sys.path.append( basedir  + "/common" )
+
+from rcs3functions import delete_user_keys, create_user_key
 
 
 usage="Replace IAM user access keys"
@@ -19,8 +22,7 @@ p.add_argument( "host",
         help="hostname" )
 args = p.parse_args()
 
-#print( args.user, args.host )
-with open( "config/aws-settings.yaml", "r" ) as f:
+with open( basedir + "/config/aws-settings.yaml", "r" ) as f:
     aws = yaml.safe_load( f )
 
 # override location of .aws/config
@@ -29,10 +31,19 @@ if "configfile" in aws:
 
 acctname = args.user +  "-" + args.host + "-sa"
 keyfile = aws[ "outputdir" ] + args.user + "-" + args.host + ".credentials"
-session = boto3.Session( profile_name=aws[ "profile" ] )
+
+# when run from AWS services, profile is not used
+if "profile" in aws:
+    session = boto3.Session( profile_name=aws[ "profile" ] )
+else:
+    session = boto3
+
+if "region" in aws:
+    iam_client = session.client( "iam", region_name=aws[ "region" ] )
+else:
+    iam_client = session.client( "iam" )
 
 # remove user keys, create new key, save to file
-iam_client = session.client( "iam" )
 try:
     delete_user_keys( iam_client, acctname )
     userkey = create_user_key( iam_client, acctname )
