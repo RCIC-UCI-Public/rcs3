332751ad4900bc35027b27711797992c32885ad7
Philip Papadopoulos <ppapadop@uci.edu>
2024-04-19 15:01:30 -0700

    Add a lifecycle (30 day delete) to inventory buckets Move lifecycle template json files to their own (lifecyle) subdirectory of templates

--- POC/cloudadmin/create-bucket-with-inventory.sh
+++ POC/cloudadmin/create-bucket-with-inventory.sh
@@ -58,6 +58,7 @@ fi
 ######################  INPUTS READ, CREATE BUCKETS, APPLY POLICIES ####################
 outputs=$OUTPUTS_DIR
 templates=$TEMPLATES_DIR
+LCD=$TEMPLATES_DIR/lifecycle
 
 user=$1
 host=$2
@@ -76,12 +77,15 @@ done
 localize $templates/template-inventory-permissions.json $outputs/$user-$host-inv-perm.json
 $AWS s3api put-bucket-policy --bucket $user-$host-$RCS3_INVENTORY_POSTFIX --policy file://$outputs/$user-$host-inv-perm.json
 
+# put the lifecycle policy into the inventory bucket to delete after 30 days deletions
+$AWS s3api put-bucket-lifecycle-configuration --bucket $user-$host-$RCS3_INVENTORY_POSTFIX --lifecycle-configuration file://$LCD/lifecycle-delete-inventory.json
+
 # put inventory configuration on the primary bucket
 localize $templates/template-inventory-configuration.json $outputs/$user-$host-inv-cfg.json
 $AWS s3api put-bucket-inventory-configuration --bucket $user-$host-$RCS3_BUCKET_POSTFIX --id $user-$host-daily --inventory-configuration file://$outputs/$user-$host-inv-cfg.json
 
 # put the lifecycle policy into the bucket for storage transititions and permanent deletions
-$AWS s3api put-bucket-lifecycle-configuration --bucket $user-$host-$RCS3_BUCKET_POSTFIX --lifecycle-configuration file://$templates/lifecycle-all-$TIERCLASS.json
+$AWS s3api put-bucket-lifecycle-configuration --bucket $user-$host-$RCS3_BUCKET_POSTFIX --lifecycle-configuration file://$LCD/lifecycle-all-$TIERCLASS.json
 
 # create service account access policy, service account, and attach policy
 $DRYRUN $MYDIR/create-service-account-policy.py $user $host $OVERRIDE_NETWORKS
