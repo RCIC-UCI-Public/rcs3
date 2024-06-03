410db0c1b6978586f6396b351c6ea7156fd361bc
Francisco Lopez <lopez@uci.edu>
2024-06-03 12:35:00 -0700

    specify save directory for dynamodb import

--- POC/scripts/lambda-createAthenaQueries.py
+++ POC/scripts/lambda-createAthenaQueries.py
@@ -5,8 +5,9 @@ import datetime
 def lambda_handler(event, context):
     # create the SQL query to load a specific S3 inventory
     # expects TableName, BackupBucket, InventoryBucket, and HiveDir as inputs
-    hivedate = datetime.date.today()
-    hivedir = event[ "HiveDir" ].format( str( hivedate ) )
+    stamp =  datetime.datetime.today()
+    
+    hivedir = event[ "HiveDir" ].format( stamp.strftime( "%Y-%m-%d" ) )
     # it is possible today's inventory run has not completed yet
     # if hivedir/symlink.txt does not exist, then use previous day's inventory run
     hivesym = "{}symlink.txt".format( hivedir )
@@ -17,8 +18,8 @@ def lambda_handler(event, context):
             Key=hivesym
         )
     except s3.exceptions.ClientError:
-        yesterday = hivedate - datetime.timedelta( days=1 )
-        hivedir = event[ "HiveDir" ].format( str( yesterday ) )
+        yesterday = stamp - datetime.timedelta( days=1 )
+        hivedir = event[ "HiveDir" ].format( yesterday.strftime( "%Y-%m-%d" ) )
     
     loadschema = "CREATE EXTERNAL TABLE {} ( bucketname string, filename string, version_id string, is_latest boolean, is_delete_marker boolean, filesize string, last_modified_date string, storage_class string ) ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde' STORED AS INPUTFORMAT 'org.apache.hadoop.hive.ql.io.SymlinkTextInputFormat' OUTPUTFORMAT  'org.apache.hadoop.hive.ql.io.IgnoreKeyTextOutputFormat' LOCATION 's3://{}/{}' ;"
     QuerySchema=loadschema.format( event[ "TableName" ], event[ "InventoryBucket" ], hivedir )
@@ -30,7 +31,11 @@ def lambda_handler(event, context):
         s = "select bucketname as \"{}\", filename, version_id from {} where filename like '{}' and (storage_class = 'GLACIER' or storage_class = 'DEEP_ARCHIVE')".format( event[ "BackupBucket" ], event[ "TableName" ], request )
         a.append( { "SearchString": s } )
     
+    # need unique save location for dynamodb upload
+    savedir = stamp.strftime( "restore%Y%m%d-%H%M%S" )
+    
     return {
         "QuerySchema": QuerySchema,
-        "QueryInventory": a
+        "QueryInventory": a,
+        "ResultsDir": savedir
     }
