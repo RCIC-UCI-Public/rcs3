12e846e749dd0f1af08dc3c4742a91e6e141eb9b
Philip Papadopoulos <ppapadop@uci.edu>
2024-09-11 17:39:17 -0700

    Database changes. csv and json filters

--- POC/tinkering/createdb.py
+++ POC/tinkering/createdb.py
@@ -40,27 +40,15 @@ def createDB ( dbname ):
                 MODE INTEGER DEFAULT 0x1C0,
                 JMTIME REAL,
                 JATIME REAL,
-                SIZE INTEGER DEFAULT 0);"""
-                                                            
-    cursor_obj.execute(filetable)
-    # Creating table OBJECTS
-    cursor_obj.execute("DROP TABLE IF EXISTS OBJECTS")
-    objecttable = """ CREATE TABLE OBJECTS (
-                OBJID VARCHAR(50) PRIMARY KEY NOT NULL,
-                FILEID INTEGER NOT NULL,
-                CLASSID INTEGER,
+                SIZE INTEGER DEFAULT 0,
+                OBJECTID VARCHAR(50) NOT NULL,
                 CURRENT INTEGER DEFAULT 1,
                 DELETEMARK INTEGER DEFAULT 1,
+                STORCLASS VARCHAR(16) DEFAULT 'LOCAL',
                 RESTORED INTEGER DEFAULT 0); """
                                                             
-    cursor_obj.execute(objecttable)
+    cursor_obj.execute(filetable)
     
-    # Create table STORAGECLASS for Enumerated S3 Storage types
-    classtable = """ CREATE TABLE STORAGECLASS (
-                ID INTEGER PRIMARY KEY,
-                CLASS VARCHAR(32) );"""
-    cursor_obj.execute(classtable)
-
     # FILE/FOLDER VIEW
     cursor_obj.execute("DROP VIEW IF EXISTS ALLFILES")
     allfilesview = """ CREATE VIEW ALLFILES AS 
@@ -68,30 +56,9 @@ def createDB ( dbname ):
     cursor_obj.execute(allfilesview)
     cursor_obj.execute("DROP VIEW IF EXISTS ALLOBJECTS")
     allobjectsview = """ CREATE VIEW ALLOBJECTS AS 
-                SELECT OBJID,FOLDER,FILENAME,UID,GID,MODE,JMTIME,SIZE,
-                CURRENT,DELETEMARK,STORAGECLASS.CLASS,RESTORED FROM 
-                OBJECTS INNER JOIN ALLFILES ON OBJECTS.FILEID=ALLFILES.ID 
-                INNER JOIN STORAGECLASS ON OBJECTS.CLASSID=STORAGECLASS.ID ;"""
+                SELECT fi.ID,fi.OBJECTID,fo2.FOLDER as LEVEL1,fo.FOLDER,fi.FILENAME,fi.SIZE,fi.jmtime,fi.CURRENT,fi.DELETEMARK,fi.STORCLASS,fi.RESTORED from FILES fi INNER JOIN FOLDERS fo ON fi.FOLDERID=fo.ID INNER JOIN FOLDERS fo2 on fo2.ID=fi.ancestorid;""" 
     
     cursor_obj.execute(allobjectsview)
-
-    ## Insert the basic enumerated storage types into Storage Class
-    cursor_obj.execute('INSERT INTO STORAGECLASS(CLASS) VALUES("STANDARD")') 
-    cursor_obj.execute("INSERT INTO STORAGECLASS(CLASS) VALUES('REDUCED_REDUNDANCY')") 
-    cursor_obj.execute("INSERT INTO STORAGECLASS(CLASS) VALUES('STANDARD_IA')") 
-    cursor_obj.execute("INSERT INTO STORAGECLASS(CLASS) VALUES('ONEZONE_IA')") 
-    cursor_obj.execute("INSERT INTO STORAGECLASS(CLASS) VALUES('INTELLIGENT_TIERING')") 
-    cursor_obj.execute("INSERT INTO STORAGECLASS(CLASS) VALUES('GLACIER')") 
-    cursor_obj.execute("INSERT INTO STORAGECLASS(CLASS) VALUES('DEEP_ARCHIVE')") 
-    cursor_obj.execute("INSERT INTO STORAGECLASS(CLASS) VALUES('GLACIER_IR')") 
-    cursor_obj.execute("INSERT INTO STORAGECLASS(CLASS) VALUES('SNOW')") 
-    connection_obj.commit()
-    
-    cursor_obj.execute("CREATE INDEX DIRNAMES on FOLDERS(folder)")
-    cursor_obj.execute("PRAGMA journal_mode=memory")
-    cursor_obj.execute("PRAGMA synchronous=off")
-    cursor_obj.execute("PRAGMA foreign_keys=on")
-    connection_obj.commit()
 
     print("Tables are Ready")
     # Close the connection
--- POC/tinkering/filtercsv.py
+++ POC/tinkering/filtercsv.py
@@ -4,10 +4,18 @@ import os
 import csv
 
 if __name__ == "__main__":
+
     records = 0
     processed=100000
     processedHuman=1000000
     phCount=0
+
+
+    insfolderpat = 'INSERT INTO FOLDERS(FOLDER) VALUES("%s");' 
+    insfilepat = 'INSERT INTO FILES(ancestorid,folderid,filename,size,jmtime,current,deletemark,storclass,objectid) values(%s,%s,"%s",%d,julianday("%s"),%d,%d,"%s","%s");' 
+
+    objectcount = 0;
+    objectbatch = 10000;
 
     # Write out some debugging information
     sys.stderr.write("Record Progress every %d records\n" % processed)
@@ -16,6 +24,7 @@ if __name__ == "__main__":
 
     csv_reader = csv.reader(sys.stdin)
     pathset=set()
+    print ("BEGIN TRANSACTION;")
     for row in csv_reader:
         records += 1
         if records % processed == 0:
@@ -26,29 +35,41 @@ if __name__ == "__main__":
                  sys.stderr.write("\n%3dM "% phCount)
 
         # Process each record here
-        bucket,prefix,oid,current,deletemark,size,modtime,storclass = row
+        try:
+           bucket,prefix,oid,current,deletemark,size,modtime,storclass = row
+        except:
+           sys.stderr.write("Error reading row %d: %s\n" % (records,str(row)))
+           sys.exit(-1) 
         
         # Break the filename and path
         fname=os.path.basename(prefix)
         folder=os.path.dirname(prefix)
         level1 = folder.split('/',2)[0]
 
-        if level1 not in pathset:
-            print('insert into folders(folder) values("%s");' % (level1))
-            pathset.add(level1)
-        if folder not in pathset:
-            print('insert into folders(folder) values("%s");' % (folder))
-            pathset.add(folder)
+        try:
+            if level1 not in pathset:
+                print(insfolderpat % (level1))
+                pathset.add(level1)
+            if folder not in pathset:
+                print(insfolderpat % (folder))
+                pathset.add(folder)
+        except:
+            sys.stderr.write("folder error with (%d): %s\n " % (records,str(row)))
 
         subselect='(select ID from FOLDERS where folder="%s")' % folder 
         subselect2='(select ID from FOLDERS where folder="%s")' % level1
-        subselect3='(select ID from STORAGECLASS where CLASS="%s")' % storclass
-        print('insert into files(ancestorid,folderid,filename,size,jmtime) values(%s,%s,"%s",%d,julianday("%s"));' % 
-                    (subselect2,subselect,fname,int(size),modtime))
-
-        subselect3='(select ID from allfiles where folder="%s" and filename="%s")' %(folder,fname)
-        subselect4='(select ID from STORAGECLASS where CLASS="%s")' % storclass
-        print('insert into objects(OBJID,FILEID,CLASSID,CURRENT,DELETEMARK) values("%s",%s,%s,%d,%d);' %
-                 (oid,subselect3,subselect4,1 if current == 'true'  else 0,
-                  1 if deletemark == 'true'  else 0)) 
+        if len(size) == 0:
+           nsize = 0;
+        else:
+           nsize = int(size)
+        try: 
+            icurrent = 1 if current == 'true' else 0
+            ideletemark = 1 if deletemark == 'true'  else 0
+            print( insfilepat % \
+               (subselect2,subselect,fname,nsize,modtime,
+               icurrent,ideletemark,storclass,oid))
+        except:
+            sys.stderr.write("error with (%d): %s\n " % (records,str(row)))
+    print ("END TRANSACTION;")
+    sys.stderr.write(" %d \n"% records)
 
