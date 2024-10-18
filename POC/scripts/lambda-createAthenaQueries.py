9447cbdd28443ce987e3a4beab3aa3eb5504cfbe
Francisco Lopez <lopez@uci.edu>
2024-10-18 16:04:05 -0700

    convert hyphens to underscore in user, host for database, table name

--- POC/scripts/lambda-createAthenaQueries.py
+++ POC/scripts/lambda-createAthenaQueries.py
@@ -4,9 +4,13 @@ import datetime
 
 def lambda_handler(event, context):
     # create the SQL query to load a specific S3 inventory
-    # expects TableName, BackupBucket, InventoryBucket, ReportsDir, and HiveDir as inputs
-    stamp =  datetime.datetime.today()
+    # expects UserName, HostName, BackupBucket, InventoryBucket, ReportsDir, and HiveDir as inputs
     
+    # convert hypens to underscores
+    QueryDatabase = event[ "UserName" ].replace( "-", "_" )
+    QueryTable = event[ "HostName" ].replace( "-", "_" )
+    
+    stamp =  datetime.datetime.today()
     hivedir = event[ "HiveDir" ].format( stamp.strftime( "%Y-%m-%d" ) )
     # it is possible today's inventory run has not completed yet
     # if hivedir/symlink.txt does not exist, then use previous day's inventory run
@@ -21,20 +25,22 @@ def lambda_handler(event, context):
         yesterday = stamp - datetime.timedelta( days=1 )
         hivedir = event[ "HiveDir" ].format( yesterday.strftime( "%Y-%m-%d" ) )
     
+    # create the SQL query to load the inventory schema
     loadschema = "CREATE EXTERNAL TABLE {} ( bucketname string, filename string, version_id string, is_latest boolean, is_delete_marker boolean, filesize string, last_modified_date string, storage_class string ) ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde' STORED AS INPUTFORMAT 'org.apache.hadoop.hive.ql.io.SymlinkTextInputFormat' OUTPUTFORMAT  'org.apache.hadoop.hive.ql.io.IgnoreKeyTextOutputFormat' LOCATION 's3://{}/{}' ;"
-    QuerySchema=loadschema.format( event[ "TableName" ], event[ "InventoryBucket" ], hivedir )
+    QuerySchema=loadschema.format( QueryTable, event[ "InventoryBucket" ], hivedir )
     
     # need unique save location for dynamodb upload
     savedir = "s3://" + event[ "ReportsDir" ] + stamp.strftime( "restore%Y%m%d-%H%M%S" )
     
     # create the SQL queries for a specific S3 inventory in Athena
-    # expects TableName, BackupBucket, and RestoreList as inputs
     a = []
+    t = "select bucketname as \"{}\", filename, version_id from {} where filename like '{}' and (storage_class = 'GLACIER' or storage_class = 'DEEP_ARCHIVE')"
     for request in event[ "RestoreList" ]:
-        s = "select bucketname as \"{}\", filename, version_id from {} where filename like '{}' and (storage_class = 'GLACIER' or storage_class = 'DEEP_ARCHIVE')".format( event[ "BackupBucket" ], event[ "TableName" ], request )
+        s = t.format( event[ "BackupBucket" ], QueryTable, request )
         a.append( { "SearchString": s, "ResultsDir": savedir } )
     
     return {
+        "QueryDatabase": QueryDatabase,
         "QuerySchema": QuerySchema,
         "QueryInventory": {
           "QueryList": a,
