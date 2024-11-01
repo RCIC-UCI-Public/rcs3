551f34ab3a79fa5d82dd611d5394b754092b352e
Francisco Lopez <lopez@uci.edu>
2024-11-01 14:59:35 -0700

    do not delete metadata, will handle in separate lambda

--- POC/scripts/lambda-createS3BatchInput.py
+++ POC/scripts/lambda-createS3BatchInput.py
@@ -5,7 +5,6 @@ import boto3
 def lambda_handler(event, context):
     # take an array of Athena search results and extract ETag and convert to
     # ARN string to feed to S3 Batch createJob step function
-    # remove metadata file before dynamodb import
     # expected format of ResultsFile is s3://bucketname/path-to-object/object
     # requires GetObject permissions on records bucket
     # limit FileToken to 64 characters, needed for CreateJob idempotency
@@ -24,8 +23,6 @@ def lambda_handler(event, context):
                     'FileToken': m.group(2)[:64],
                     'ExpireDays': d
                 } )
-                metadata = m.group(2) + ".metadata"
-                r = s3.delete_object( Bucket=m.group(1), Key=metadata )
             else:
                 l.append( {
                     'ResultsFile': n[ "ResultsFile" ],
