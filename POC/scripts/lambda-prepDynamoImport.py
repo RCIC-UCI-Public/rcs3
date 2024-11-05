4b77e2b8890421ab00d27b4a19a33e853d090fed
Francisco Lopez <lopez@uci.edu>
2024-11-04 16:42:42 -0800

    new lambda to remove non cvs files for DynamoDB import

---
+++ POC/scripts/lambda-prepDynamoImport.py
@@ -0,0 +1,24 @@
+import re
+import boto3
+
+def lambda_handler(event, context):
+    # remove .txt and .metadata files from directory
+    # remaining .cvs files will be imported by DynamoDB
+    b = event[ "Bucket" ]
+    p = event[ "DataDir" ]
+    k = []
+    d = []
+    s3 = boto3.client( "s3" )
+    response = s3.list_object_versions( Bucket=b, Prefix=p )
+    for obj in response[ "Contents" ]:
+        key = obj[ "Key" ]
+        if re.search( "\.cvs$", key )
+            # keep this file
+            k.append( { "Key": key } )
+        else
+            # delete this file
+            d.append( { "Key": key } )
+    
+    #s3.delete_objects( Bucket=b, Delete={ 'Objects': d, 'Quiet': True } )
+    
+    return { "KeepFiles": k, "DeleteFiles": d }
