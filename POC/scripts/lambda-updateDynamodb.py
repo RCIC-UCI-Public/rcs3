2190f4782628d34866071cd7bba91dc20c64084f
Francisco Lopez <lopez@uci.edu>
2024-11-06 15:54:53 -0800

    switch from resource to client

--- POC/scripts/lambda-updateDynamodb.py
+++ POC/scripts/lambda-updateDynamodb.py
@@ -7,26 +7,40 @@ def lambda_handler(event, context):
     filename = event['detail']['object']['key']
     version_id = event['detail']['object']['version-id']
     eventtime = event['time']
-
-    dynamodb = boto3.resource( "dynamodb" )
+    
+    dynamodb = boto3.client( "dynamodb" )
     # assumes table name is also bucket name
-    table = dynamodb.Table( bucket )
-
-    get_response = table.get_item(
-        Key={ 'filename': filename, 'version_id': version_id},
+    table = bucket
+    
+    try:
+        get_response = dynamodb.get_item(
+            TableName=table,
+            Key={ 'filename': { 'S': filename }, 'version_id': { 'S': version_id } }
         )
         
-    if len(get_response) == 1:
-        print("object does not exist")
-    else:
-        restore_starttime = datetime.datetime.strptime(get_response['Item']['restore_init_time'], '%Y-%m-%dT%H:%M:%S%fZ')
-        restore_finishtime = datetime.datetime.strptime(eventtime, '%Y-%m-%dT%H:%M:%S%fZ')
-        restore_delta = restore_finishtime - restore_starttime
-        restore_duration = restore_delta.total_seconds()/60
-
-        update_response =  table.update_item(
-            Key={ 'filename': filename, 'version_id': version_id},
-            UpdateExpression="set restore_completed = :s, restore_comp_time = :c, restore_duration = :e, object_accessible = :val1",
-            ExpressionAttributeValues={':s': "yes", ':c': eventtime, ':e': int(restore_duration), ':val1': "yes"},
-            ConditionExpression='attribute_exists(filename) and attribute_exists(version_id)',
-            ReturnValues="UPDATED_NEW")
+        if not 'Item' in get_response:
+            print("object does not exist")
+        else:
+            #restore_starttime = datetime.datetime.strptime(get_response['Item']['restore_init_time'], '%Y-%m-%dT%H:%M:%S%fZ')
+            #restore_finishtime = datetime.datetime.strptime(eventtime, '%Y-%m-%dT%H:%M:%S%fZ')
+            #restore_delta = restore_finishtime - restore_starttime
+            #restore_duration = restore_delta.total_seconds()/60
+            #
+            #update_response =  table.update_item(
+            #    Key={ 'filename': filename, 'version_id': version_id},
+            #    UpdateExpression="set restore_completed = :s, restore_comp_time = :c, restore_duration = :e, object_accessible = :val1",
+            #    ExpressionAttributeValues={':s': "yes", ':c': eventtime, ':e': int(restore_duration), ':val1': "yes"},
+            #    ConditionExpression='attribute_exists(filename) and attribute_exists(version_id)',
+            #    ReturnValues="UPDATED_NEW")
+            update_response =  dynamodb.update_item(
+                TableName=table,
+                Key={ 'filename': { 'S': filename }, 'version_id': { 'S': version_id } },
+                UpdateExpression="set #RC = :s",
+                ExpressionAttributeNames={ '#RC': bucket },
+                ExpressionAttributeValues={ ':s': { "S": "yes" } },
+                ConditionExpression='attribute_exists(filename) and attribute_exists(version_id)',
+                ReturnValues="UPDATED_NEW"
+            )
+    except dynamodb.exceptions.ResourceNotFoundException:
+        # okay if table does not exist
+        pass
